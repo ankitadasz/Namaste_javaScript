bbf5d1fee8af56da195d06e01af4c581d6c31e82
Ankita Das <daszankita23@gmail.com>
2025-07-21 14:01:36 +0530

    PromiseAPIS in js

--- Season-2/05/script.js
+++ Season-2/05/script.js
@@ -7,28 +7,33 @@ const p2= new Promise((resolve,reject)=>{
 const p3= new Promise((resolve,reject)=>{
     setTimeout(()=>reject("p3 success"),4000)
 })
-// Promise.all([p1,p2,p3]).then(res=>{
-//     console.log(res);
-// })
-// .catch((err)=>{
-//     console.error("error is:",err);
-    
-// })
 
-// Promise.allSettled([p1,p2,p3]).then(res=>{
-//     console.log(res);
-// })
-// .catch((err)=>{
-//     console.error("error is:",err);
+
+/////////////////////////////////////////////////////////////////////////
+Promise.all([p1,p2,p3]).then(res=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.error("error is:",err);
     
-// })
-// Promise.race([p1,p2,p3]).then(res=>{
-//     console.log(res);
-// })
-// .catch((err)=>{
-//     console.error("error is:",err);
+})
+/////////////////////////////////////////////////////////////////////////
+Promise.allSettled([p1,p2,p3]).then(res=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.error("error is:",err);
     
-// })
+})
+/////////////////////////////////////////////////////////////////////////
+Promise.race([p1,p2,p3]).then(res=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.error("error is:",err);
+    
+})
+//////////////////////////////////////////////////////////////////////////
 Promise.any([p1,p2,p3]).then(res=>{
     console.log(res);
 })
